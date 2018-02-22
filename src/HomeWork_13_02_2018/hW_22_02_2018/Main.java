c132c6e4de50a76da8f1da209232078becbd4614
cthutq <cthutq2511@gmail.com>
2018-02-23 00:18:59 +0200

    HomeWork 22.02.2018

---
+++ src/HomeWork_13_02_2018/hW_22_02_2018/Main.java
@@ -0,0 +1,32 @@
+package HomeWork_13_02_2018.hW_22_02_2018;
+
+public class Main {
+
+    // метод Фибоначи
+    static int fibonachi(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibonachi(n - 1) + fibonachi(n - 2);
+    }
+//==========================================================
+
+    // Рекурсия
+    static void countDownRec(int numRec) {
+        if (numRec < 0) return;
+        System.out.print(numRec-- + "       ");
+        countDownRec(numRec);
+    }
+//==========================================================
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 25; i++) {
+            System.out.print(fibonachi(i) + " ");
+            }
+        System.out.println();
+            countDownRec(25);
+
+    }
+
+
+}
